b8b573f45c3c0ce2dd75af84aec4d6d1262a0f03
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-07-26 22:34:46 +0300

    reactodev

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,24 +1,41 @@
-import { data } from "@/data";
+'use client';
 import Image from "next/image";
 import style from "./page.module.css";
+import { use, useEffect } from "react";
+
+async function fetchData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    return data;
+  }
+  catch(error) {
+    console.error("Veri alınırken bir hata oluştu:", error);
+  }
+}
 
 export default async function SingleBlogPage({ params }) {
-  const { id } = await params;
-
-  const blog = data.filter((blog) => blog.id === id)[0];
-
+ const [data,setData] =useState([]);
+useEffect(()=>{
+  fetchData().then((data)=>{
+    setData(data);
+    console.log("Veri başarıyla alındı:", data);
+  });
+},[]);
   return (
     <div>
-      <Image
-        src={blog.image}
+      {data.map((item)=>(
+        <Image
+       src={"https://via.placeholder.com/600x400"}
         alt=""
         width={0}
         height={0}
         sizes="100vw"
         className={style.image}
-      />
-      <h1>{blog.title}</h1>
-      <p>{blog.content}</p>
-    </div>
+        />
+      <h1>{item.title}</h1>
+    
+      ))}
+      </div>
   );
 }
--- app/blog/page.js
+++ app/blog/page.js
@@ -1,19 +1,40 @@
+'use client';
 import { BlogCard } from "@/components/BlogCard";
-import { data } from "@/data";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+
+async function fetchData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Veri alınırken bir hata oluştu:", error);
+    return []; // Hata durumunda boş array dönelim ki hata olmasın
+  }
+}
 
 export default function BlogPage() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData().then((data) => {
+      console.log("Veri başarıyla alındı:", data);
+      setData(data);
+    });
+  }, []);
+
   return (
     <div>
       <h1>Blog Sayfası</h1>
       <p>Bu, blog sayfasının içeriğidir.</p>
       <div className={styles.blogList}>
-        {data.map((blog) => (
+        {data.map((item) => (
           <BlogCard
-            key={blog.id}
-            id={blog.id}
-            title={blog.title}
-            imageUrl={blog.image}
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            imageUrl={"https://via.placeholder.com/150"} // Sabit placeholder görsel koydum
           />
         ))}
       </div>
